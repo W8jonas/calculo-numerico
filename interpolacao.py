c760baccc556500d57f482e99001ed49f75d3d44
W8jonas <jonashenrique989@gmail.com>
2023-12-20 22:29:38 -0300

    Calculando interpolação corretamente

--- interpolacao.py
+++ interpolacao.py
@@ -14,39 +14,29 @@ def ler_dados_de_arquivo():
     return n, pontosXY
 
 
+def calcula_por_lagrange(nn, pontos, pontoACalcular):
+    yp = 0
+    for k in range(0, nn):
+        p = 1
+        for j in range(0, nn):
+            if (k != j):
+                p = p * (pontoACalcular - pontos[j][0]) / (pontos[k][0] - pontos[j][0])
+        yp = yp + p * pontos[k][1]
 
-def calcular_coeficientes_diferenca_dividida(num_pontos, pontos):
-    coeficientes = [pontos[i][1] for i in range(num_pontos)]
-    for j in range(1, num_pontos):
-        for i in range(num_pontos - 1, j - 1, -1):
-            coeficientes[i] = (coeficientes[i] - coeficientes[i - 1]) / (pontos[i][0] - pontos[i - j][0])
-    return coeficientes
-
-
-def calcular_interpolar_polinomio_newton(num_pontos, x_interpolacao, pontos):
-    coeficientes = calcular_coeficientes_diferenca_dividida(num_pontos, pontos)
-    resultado = coeficientes[0]
-    produto = 1
-    for i in range(1, len(coeficientes)):
-        produto *= (x_interpolacao - pontos[i - 1][0])
-        resultado += coeficientes[i] * produto
-    return resultado
-
+    return yp
 
 
 num_pontos, pontos = ler_dados_de_arquivo()
 
-print("Pontos lidos:")
+print("\nPontos lidos:")
 for ponto in pontos:
     print(ponto)
 
-x_interpolacao = float(input("Digite o ponto para interpolação: "))
+x_teclado = float(input("Digite o ponto para interpolação: "))
 
-if x_interpolacao < pontos[0][0] or x_interpolacao > pontos[-1][0]:
+if x_teclado < pontos[0][0] or x_teclado > pontos[-1][0]:
     print("O ponto de interpolação está fora do intervalo fornecido.")
 else:
-    resultado_interpolado = calcular_interpolar_polinomio_newton(num_pontos, x_interpolacao, pontos)
-    print(f"O valor interpolado em x = {x_interpolacao} é: {resultado_interpolado}")
-
-
+    resultado_interpolado = calcula_por_lagrange(num_pontos, pontos, x_teclado)
+    print(f"O valor interpolado em x = {x_teclado} é: {resultado_interpolado}")
 
